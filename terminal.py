dcc694d17741b26aee52a8648dc638725ae14c1b
Stefan Kauerauf <stefan.kauerauf@shd-online.de>
2015-07-07 15:26:54 +0200

    added glade

--- terminal.py
+++ terminal.py
@@ -3,20 +3,68 @@
 from gi.repository import Gtk, Gdk, Vte, GLib
 import os
 
-terminal = Vte.Terminal()
-terminal.spawn_sync(
-    Vte.PtyFlags.DEFAULT,
-    os.environ['HOME'],
-    ["/bin/bash"],
-    [],
-    GLib.SpawnFlags.DO_NOT_REAP_CHILD,
-    None,
-    None,
-    )
 
-win = Gtk.Window()
-win.connect('delete-event', Gtk.main_quit)
-win.add(terminal)
-win.show_all()
+class Handler:
 
-Gtk.main()
+    def onDeleteWindow(self, *args):
+        Gtk.main_quit(*args)
+
+
+class Terminal:
+
+    def __init__(self):
+        self.builder = Gtk.Builder()
+        self.clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+        self.builder.add_from_file("terminal.glade")
+        self.builder.connect_signals(Handler())
+
+    def run(self):
+        window = self.builder.get_object("window1")
+        window.set_title("terminal")
+        window.set_default_size(480, 340)
+
+        terminal = Vte.Terminal()
+        terminal.connect("child-exited", Handler.onDeleteWindow)
+
+        terminal.spawn_sync(
+            Vte.PtyFlags.DEFAULT,
+            os.environ['HOME'],
+            ["/bin/bash"],
+            [],
+            GLib.SpawnFlags.DO_NOT_REAP_CHILD,
+            None,
+            None,
+            )
+
+        window.add(terminal)
+        window.show_all()
+        Gtk.main()
+
+
+
+
+if __name__ == "__main__":
+    app = Terminal()
+    app.run()
+
+
+
+
+
+#terminal = Vte.Terminal()
+#terminal.spawn_sync(
+#    Vte.PtyFlags.DEFAULT,
+#    os.environ['HOME'],
+#    ["/bin/bash"],
+#    [],
+#    GLib.SpawnFlags.DO_NOT_REAP_CHILD,
+#    None,
+#    None,
+#    )
+
+#win = Gtk.Window()
+#win.connect('delete-event', Gtk.main_quit)
+#win.add(terminal)
+#win.show_all()
+
+#Gtk.main()
